7e37ec100ad49960ae8965e7b610cfbae17782b3
andrey31453 <andrey31453@google.com>
2024-02-28 16:10:14 +0300

    f

---
+++ webpack.config.from_manifest.js
@@ -0,0 +1,20 @@
+// не переписывать
+// данный файл сгенерирован автоматически
+
+const src = path.join(__dirname, 'src/')
+
+module.exports = ({ dev }) => ({
+	plugins: [
+		new module_federation({
+      name: 'app1',
+      filename: 'remote_entry.js',
+			exposes: ,
+			remotes: ,
+      shared: {
+				'react': { singleton: true },
+        'react-dom': { singleton: true },
+				
+      },
+    }),
+	],
+})
