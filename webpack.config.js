951937f5e8bcd2e482df83b038a2866e583b7e86
andrey31453 <andrey31453@google.com>
2024-02-29 16:15:44 +0300

    f

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { merge } = require('webpack-merge')
 
 const template = require('./webpack.config.template')
 const generate = require('./webpack.config.from_manifest')
-		const local = require('./webpack.config.local')
+const local = require('./webpack.config.local')
 
 module.exports = (props) =>
   merge(template(props), generate(props), local(props))
